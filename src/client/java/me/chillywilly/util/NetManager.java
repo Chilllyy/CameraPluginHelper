18549b7e370001b7e75a4b01c5730b6d25d04136
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2024-11-25 12:40:26 -0700

    Implemented packet return when companion check is recieved

--- src/client/java/me/chillywilly/util/NetManager.java
+++ src/client/java/me/chillywilly/util/NetManager.java
@@ -2,13 +2,22 @@ package me.chillywilly.util;
 
 import me.chillywilly.CameraPluginHelper;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 
 public class NetManager {
     public static void init() {
         ClientPlayNetworking.registerGlobalReceiver(NetConst.SCREENSHOT_PACKET_ID, (client, handler, buf, responseSender) -> {
             buf.readByte();
             int auth = buf.readInt();
-            CameraPluginHelper.LOGGER.info("Recieved Packet: {}", auth);
+            CameraPluginHelper.LOGGER.info("Received screenshot packed, Auth ID {}", auth);
+            //TODO screenshot & return SCREENSHOT_TAKEN_ID packet
+        });
+
+        ClientPlayNetworking.registerGlobalReceiver(NetConst.CHECK_FOR_COMPANION_ID, (client, handler, buf, responseSender) -> {
+            CameraPluginHelper.LOGGER.info("Received companion check request, responding now");
+
+            //sends response packet so the server knows that the client has the mod
+            ClientPlayNetworking.send(NetConst.COMPANION_FOUND_ID, PacketByteBufs.empty());
         });
     }
 }
