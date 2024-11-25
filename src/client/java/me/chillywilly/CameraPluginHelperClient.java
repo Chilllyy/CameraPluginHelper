48127e235b064e71296a5775d98fe9c1ece93256
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2024-11-25 12:40:07 -0700

    removed Hello World Log

--- src/client/java/me/chillywilly/CameraPluginHelperClient.java
+++ src/client/java/me/chillywilly/CameraPluginHelperClient.java
@@ -6,7 +6,8 @@ import net.fabricmc.api.ClientModInitializer;
 public class CameraPluginHelperClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		CameraPluginHelper.LOGGER.info("Hello World, from chilly's second mod!");
 		NetManager.init();
 	}
+
+
 }
