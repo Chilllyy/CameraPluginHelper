be3f2ce67ae3fb4960c48cb5554185d39f0740fd
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2024-11-25 19:29:00 -0700

    Added code to remove hand and HUD when enabled

---
+++ src/client/java/me/chillywilly/mixin/client/RemoveHud.java
@@ -0,0 +1,19 @@
+package me.chillywilly.mixin.client;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import me.chillywilly.CameraPluginHelper;
+import net.minecraft.client.gui.hud.InGameHud;
+
+@Mixin(InGameHud.class)
+public class RemoveHud {
+    @Inject(at=@At("HEAD"), method = "render", cancellable = true)
+    public void render(CallbackInfo info) {
+        if (CameraPluginHelper.remove) {
+            info.cancel();
+        }
+    }
+}
