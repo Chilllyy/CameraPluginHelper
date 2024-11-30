888d95f7a16719625ef99ca121432222adaf71c2
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2024-11-29 17:12:27 -0700

    Added code to remove hand and GUI

--- src/client/java/me/chillywilly/mixin/client/RemoveHand.java
+++ src/client/java/me/chillywilly/mixin/client/RemoveHand.java
@@ -5,15 +5,12 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import me.chillywilly.CameraPluginHelper;
 import net.minecraft.client.render.item.HeldItemRenderer;
 
 @Mixin(HeldItemRenderer.class)
 public class RemoveHand {
     @Inject(at=@At("HEAD"), cancellable = true, method = "renderItem(FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider$Immediate;Lnet/minecraft/client/network/ClientPlayerEntity;I)V")
     public void render(CallbackInfo info) {
-        if (CameraPluginHelper.remove) {
-            info.cancel();
-        }
+        info.cancel();
     }
 }
