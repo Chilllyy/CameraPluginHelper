758f9c5f8017394364f64a594ddd1e3635d6ceff
Chilllyy <chillywillymc@gmail.com>
2026-04-07 19:54:14 -0600

    remove No Pause and remove Hud Mixins, now handled in NetManager

--- src/client/java/me/chillywilly/CameraPluginHelperClient.java
+++ src/client/java/me/chillywilly/CameraPluginHelperClient.java
@@ -4,7 +4,6 @@ import me.chillywilly.util.NetManager;
 import net.fabricmc.api.ClientModInitializer;
 
 public class CameraPluginHelperClient implements ClientModInitializer {
-	public static boolean taking_screenshot = false;
 	@Override
 	public void onInitializeClient() {
 		NetManager.init();
